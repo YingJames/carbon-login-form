0ba6b4c209b4228df73230e87432e6cf327fe6a2
YingJames <jamesy148@usf.edu>
2023-08-04 21:28:29 -0400

    add popover

--- src/components/organisms/Navbar/Navbar.jsx
+++ src/components/organisms/Navbar/Navbar.jsx
@@ -12,48 +12,74 @@ import {
     SideNav,
     SideNavItems,
     HeaderSideNavItems,
+    Popover,
+    PopoverContent
 } from '@carbon/react';
 import { Notification, UserAvatar } from '@carbon/react/icons';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 
-const Navbar = () => (
-    <HeaderContainer
-        render={({isSideNavExpanded, onClickSideNavExpand}) => (
-            <Header aria-label="Carbon Tutorial">
-                <SkipToContent />
-                <HeaderMenuButton
-                    aria-label="Open menu"
-                    onClick={onClickSideNavExpand}
-                    isActive={isSideNavExpanded}
-                />
-                <HeaderName as={Link} to="/" prefix="">
-                    Carbon Login Form App
-                </HeaderName>
-                <HeaderNavigation aria-label="Carbon Tutorial">
-                    <HeaderMenuItem as={Link} to="/repos">Repositories</HeaderMenuItem>
-                </HeaderNavigation>
-                <SideNav
-                    aria-label="Side navigation"
-                    expanded={isSideNavExpanded}
-                    isPersistent={false}
-                >
-                    <SideNavItems>
-                        <HeaderSideNavItems>
-                            <HeaderMenuItem as={Link} to="/repos">Repositories</HeaderMenuItem>
-                        </HeaderSideNavItems>
-                    </SideNavItems>
-                </SideNav>
-                <HeaderGlobalBar>
-                    <HeaderGlobalAction aria-label="Notifications" tooltipAlignment="center">
-                        <Notification size={20} />
-                    </HeaderGlobalAction>
-                    <HeaderGlobalAction aria-label="Profile" tooltipAlignment="end">
-                        <UserAvatar size={20} />
-                    </HeaderGlobalAction>
-                </HeaderGlobalBar>
-            </Header>
-        )}
-    />
-);
+const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    return (
+        <HeaderContainer
+            render={({isSideNavExpanded, onClickSideNavExpand}) => (
+                <Header aria-label="Carbon Tutorial">
+                    <SkipToContent />
+                    <HeaderMenuButton
+                        aria-label="Open menu"
+                        onClick={onClickSideNavExpand}
+                        isActive={isSideNavExpanded}
+                    />
+                    <HeaderName as={Link} to="/" prefix="">
+                        Carbon Login Form App
+                    </HeaderName>
+                    <HeaderNavigation aria-label="Carbon Tutorial">
+                        <HeaderMenuItem as={Link} to="/repos">Repositories</HeaderMenuItem>
+                    </HeaderNavigation>
+                    <SideNav
+                        aria-label="Side navigation"
+                        expanded={isSideNavExpanded}
+                        isPersistent={false}
+                    >
+                        <SideNavItems>
+                            <HeaderSideNavItems>
+                                <HeaderMenuItem as={Link} to="/repos">Repositories</HeaderMenuItem>
+                            </HeaderSideNavItems>
+                        </SideNavItems>
+                    </SideNav>
+                    <HeaderGlobalBar>
+                        <HeaderGlobalAction aria-label="Notifications" tooltipAlignment="center">
+                            <Notification size={20} />
+                        </HeaderGlobalAction>
+                        <HeaderGlobalAction 
+                            aria-label="Account" 
+                            tooltipAlignment="end"
+                            >
+                        <Popover 
+                            open={isOpen} 
+                            align='bottom-right'
+                            onClick={() => setIsOpen(!isOpen)}  
+                        >
+                            <div className="popover-trigger">
+                                <UserAvatar size={20} />
+                            </div>
+                            <PopoverContent>
+                                <div className="popover-example-content">
+                                <p className="popover-title">Available storage</p>
+                                <p className="popover-details">
+                                    This server has 150 GB of block storage remaining.
+                                </p>
+                                </div>
+                            </PopoverContent>
+                        </Popover>
+                        </HeaderGlobalAction>
+                    </HeaderGlobalBar>
+                </Header>
+            )}
+        />
+    );
+};
 
 export default Navbar;
