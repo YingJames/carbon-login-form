7a7f05ae4216497b70641bec9ed8d2696fa2be80
James Y <jyab0617@gmail.com>
2023-08-11 11:05:13 -0400

    modify for github-pages

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-      <Router>
+      <Router basename={"https://yingjames.github.io/carbon-login-form/"}>
           <App />
       </Router>
   </React.StrictMode>
